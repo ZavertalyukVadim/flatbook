c60de50e12915b5b77e893673b0e5f38b00dba47
Vladyslav Kovtunenko <themarlboro98@gmail.com>
2017-03-14 20:58:35 +0200

    added Showroom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+import Showroom from './components/showroom'
+
+render(<Showroom/>, document.getElementById('root'));
