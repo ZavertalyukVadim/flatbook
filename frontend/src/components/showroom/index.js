f03ac7f825820efef1283d55e172a20775265423
juliakukuliak <juliakukuliak@gmail.com>
2017-03-17 15:38:27 +0200

    Buttons

--- frontend/src/components/showroom/index.js
+++ frontend/src/components/showroom/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import GridExample from '../grid';
 import Input from '../input';
 import Checkbox from '../checkbox';
+import Button from '../buttons';
 
 export default class Showroom extends Component {
     constructor(props) {
@@ -32,6 +33,47 @@ export default class Showroom extends Component {
                 <Checkbox onClick={this.onCheckboxClick} checked={this.state.checked} disabled={false}>
                     Label </Checkbox>
                 <GridExample/>
+                <div>                   
+                    <Button
+                        className='btn-success'
+                        size='large'
+                        children='Success'
+                    />
+                    
+                    <Button
+                        className='btn-danger'
+                        size='large'
+                        children='Danger'
+                    />
+                    <Button
+                        className='btn-primary'
+                        size='large'
+                        children='Primary'
+                    />
+                
+                    <Button
+                        className='btn-success'
+                        size='large'
+                        children='Large'
+                    />
+                    
+                    <Button
+                        className='btn-danger'
+                        size='medium'
+                        children='Medium'
+                    />
+                    <Button
+                        className='btn-primary'
+                        size='small'
+                        children='Small'
+                    />
+                    <Button
+                        className='btn-success'
+                        size='medium'
+                        children='Disabled'
+                        disabled={true}
+                    />
+                </div>
             </div>
         );
     }
