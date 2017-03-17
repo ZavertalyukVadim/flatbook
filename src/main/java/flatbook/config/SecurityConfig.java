a64438606339a43bf49ce981134bf7f78d1dc1eb
vadim <zavertal.v@gmail.com>
2017-03-17 20:01:40 +0200

    change config

--- src/main/java/flatbook/config/SecurityConfig.java
+++ src/main/java/flatbook/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .and().formLogin()
                 .and().exceptionHandling().accessDeniedPage("/Access_Denied");
     }
