1b0f57c5879abc513b68bae499b13965ed1a5efb
vadim <zavertal.v@gmail.com>
2017-03-20 20:01:43 +0200

    #20 rename fields

--- src/main/java/flatbook/announcement/service/AnnouncementService.java
+++ src/main/java/flatbook/announcement/service/AnnouncementService.java
@@ -12,10 +12,10 @@ import java.util.List;
 @Service
 public class AnnouncementService {
     @Autowired
-    private AnnouncementDao dao;
+    private AnnouncementDao announcementDao;
 
     public List<Announcement> getAllAnnouncement() {
-        List<Announcement> list = dao.findAll();
+        List<Announcement> list = announcementDao.findAll();
         list.sort(Comparator.comparing(Announcement::getDate));
         return list;
     }
@@ -23,40 +23,45 @@ public class AnnouncementService {
     public void test() {
         Announcement announcement = new Announcement(123, "ololo", false, new Date());
         announcement.setDate(new Date());
+        announcement.setTitle("title1");
         Announcement announcement1 = new Announcement(122, "ololo1", false, new Date());
         announcement1.setDate(new Date());
+        announcement1.setTitle("title2");
         Announcement announcement2 = new Announcement(121, "ololo2", false, new Date());
         announcement2.setDate(new Date());
+        announcement2.setTitle("title3");
         Announcement announcement3 = new Announcement(120, "ololo3", false, new Date());
         announcement3.setDate(new Date());
-        dao.save(announcement);
-        dao.save(announcement1);
-        dao.save(announcement2);
-        dao.save(announcement3);
+        announcement3.setTitle("title4");
+        announcementDao.save(announcement);
+        announcementDao.save(announcement1);
+        announcementDao.save(announcement2);
+        announcementDao.save(announcement3);
     }
 
     public Announcement getAnnouncementById(Integer id) {
-        return dao.findOne(id);
+        return announcementDao.findOne(id);
     }
 
     public Announcement updateAnnouncement(Announcement oldAnnouncement) {
-        Announcement announcement = dao.findOne(oldAnnouncement.getId());
+        Announcement announcement = announcementDao.findOne(oldAnnouncement.getId());
         announcement.setDescription(oldAnnouncement.getDescription());
-        announcement.setMarker(oldAnnouncement.getMarker());
+        announcement.setVisibility(oldAnnouncement.getVisibility());
         announcement.setPrice(oldAnnouncement.getPrice());
         announcement.setDate(new Date());
-        dao.save(announcement);
+        announcement.setTitle(oldAnnouncement.getTitle());
+        announcementDao.save(announcement);
         return announcement;
     }
 
     public Announcement deleteAnnouncement(Integer id) {
-        dao.delete(id);
+        announcementDao.delete(id);
         return null;
     }
 
     public Announcement createAnnouncement(Announcement announcement) {
         announcement.setDate(new Date());
-        dao.save(announcement);
+        announcementDao.save(announcement);
         return announcement;
     }
 }
