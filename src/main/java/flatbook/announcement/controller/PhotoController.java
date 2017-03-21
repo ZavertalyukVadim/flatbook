02df9b2900a19b29146007780dee6b36b98b95de
vadim <zavertal.v@gmail.com>
2017-03-21 18:32:38 +0200

    #20 add service and controller for Photo and Implement CRUD for him

--- src/main/java/flatbook/announcement/controller/PhotoController.java
+++ src/main/java/flatbook/announcement/controller/PhotoController.java
@@ -1,7 +1,40 @@
 package flatbook.announcement.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import flatbook.announcement.entity.Photo;
+import flatbook.announcement.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/photo")
 public class PhotoController {
+    @Autowired
+    private PhotoService photoService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Photo> getAllPhoto() {
+        return photoService.getAllPhoto();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Photo getPhotoById(@PathVariable("id") Integer id) {
+        return photoService.getPhotoById(id);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Photo createPhoto(@RequestBody Photo photo) {
+        return photoService.createPhoto(photo);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public Photo updatePhoto(@RequestBody Photo photo) {
+        return photoService.updatePhoto(photo);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Photo deletePhoto(@PathVariable("id") Integer id) {
+        return photoService.deletePhoto(id);
+    }
 }
--- src/main/java/flatbook/announcement/service/PhotoService.java
+++ src/main/java/flatbook/announcement/service/PhotoService.java
@@ -1,7 +1,40 @@
 package flatbook.announcement.service;
 
+import flatbook.announcement.dao.PhotoDao;
+import flatbook.announcement.entity.Photo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PhotoService {
+    @Autowired
+    private PhotoDao photoDao;
+
+    public List<Photo> getAllPhoto() {
+        return photoDao.findAll();
+    }
+
+    public Photo getPhotoById(Integer id) {
+        return photoDao.findOne(id);
+    }
+
+    public Photo createPhoto(Photo photo) {
+        photoDao.save(photo);
+        return photo;
+    }
+
+    public Photo deletePhoto(Integer id) {
+        photoDao.delete(id);
+        return null;
+    }
+
+    public Photo updatePhoto(Photo oldPhoto) {
+        Photo photo = photoDao.findOne(oldPhoto.getId());
+        photo.setTitle(oldPhoto.getTitle());
+        photo.setLink(oldPhoto.getLink());
+        photo.setDefaultImage(oldPhoto.getDefaultImage());
+        return photo;
+    }
 }
