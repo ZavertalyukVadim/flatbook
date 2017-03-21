417a1aee7b361226155fdec8fc7c8deaf5ba606b
vadim <zavertal.v@gmail.com>
2017-03-21 16:24:39 +0200

    #20 change fetchType in Announcement for country,region,city on EAGER and correct method test

--- src/main/java/flatbook/announcement/service/AnnouncementService.java
+++ src/main/java/flatbook/announcement/service/AnnouncementService.java
@@ -1,9 +1,16 @@
 package flatbook.announcement.service;
 
 import flatbook.announcement.dao.AnnouncementDao;
+import flatbook.announcement.dao.CityDao;
+import flatbook.announcement.dao.CountryDao;
+import flatbook.announcement.dao.RegionDao;
 import flatbook.announcement.entity.Announcement;
+import flatbook.announcement.entity.City;
+import flatbook.announcement.entity.Country;
+import flatbook.announcement.entity.Region;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.Date;
@@ -14,18 +21,44 @@ public class AnnouncementService {
     @Autowired
     private AnnouncementDao announcementDao;
 
+    @Autowired
+    private CountryDao countryDao;
+
+    @Autowired
+    private RegionDao regionDao;
+
+    @Autowired
+    private CityDao cityDao;
+
     public List<Announcement> getAllAnnouncement() {
         List<Announcement> list = announcementDao.findAll();
         list.sort(Comparator.comparing(Announcement::getLastUpdated));
         return list;
     }
-
+    @Transactional
     public void test() {
+        Country coutry = new Country();
+        coutry.setName("Ukraine");
+        countryDao.save(coutry);
+        Region region = new Region();
+        region.setName("Cherkasy");
+        region.setCountry(coutry);
+        regionDao.save(region);
+        City city = new City();
+        city.setName("Cherkasy");
+        city.setRegion(region);
+        cityDao.save(city);
         Announcement announcement = new Announcement(123, "ololo", false, new Date());
         announcement.setLastUpdated(new Date());
         announcement.setTitle("title1");
         announcement.setRooms(1);
         announcement.setLivingPlaces(100);
+        announcement.setStreet("Shevchenko");
+        announcement.setHouseNumber("450");
+        announcement.setCountry(coutry);
+        announcement.setRegion(region);
+        announcement.setCity(city);
+        announcementDao.save(announcement);
         Announcement announcement1 = new Announcement(122, "ololo1", false, new Date());
         announcement1.setLastUpdated(new Date());
         announcement1.setTitle("title2");
@@ -41,7 +74,6 @@ public class AnnouncementService {
         announcement3.setTitle("title4");
         announcement3.setRooms(1);
         announcement3.setLivingPlaces(100);
-        announcementDao.save(announcement);
         announcementDao.save(announcement1);
         announcementDao.save(announcement2);
         announcementDao.save(announcement3);
