fa1ee1825ff88c6dc3c586ba2ce81ad3db866457
vadim <zavertal.v@gmail.com>
2017-03-21 15:40:58 +0200

    #20 add bounds for Announcement add entity for Country,Region,City

---
+++ src/main/java/flatbook/announcement/entity/Region.java
@@ -0,0 +1,43 @@
+package flatbook.announcement.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "region")
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "region_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
