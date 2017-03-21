6957949a537297ac0e774289aac5b07e22dc4f81
Andrey Koval <kaonvdarley@gmail.com>
2017-03-21 16:42:37 +0200

    #35 fixed boolean issue with primary name

--- src/main/java/flatbook/profile/entity/Email.java
+++ src/main/java/flatbook/profile/entity/Email.java
@@ -5,14 +5,41 @@ import javax.persistence.*;
 @Entity
 @Table(name = "emails")
 public class Email {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "primary")
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "is_primary")
     private Boolean primary;
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Boolean getPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(Boolean primary) {
+        this.primary = primary;
+    }
 }
--- src/main/java/flatbook/profile/entity/User.java
+++ src/main/java/flatbook/profile/entity/User.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     private Integer id;
 
     @Column(name = "first_name")
@@ -23,9 +24,54 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @OneToMany(mappedBy = "user_id", fetch = FetchType.LAZY)
+    public List<Email> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<Email> emails) {
+        this.emails = emails;
+    }
+
+    @OneToMany(mappedBy = "user")
     private List<Email> emails;
 
-    @OneToMany(mappedBy = "user_id", fetch = FetchType.LAZY)
-    private List<Phone> phones;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
