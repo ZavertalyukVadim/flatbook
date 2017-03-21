b90f78cd5796b334ac20dcc99d63ecdc1dc5e75e
vadim <zavertal.v@gmail.com>
2017-03-21 18:28:02 +0200

    #20 create service and controller for amenity and add method for read all amenity

--- src/main/java/flatbook/announcement/controller/AmenityController.java
+++ src/main/java/flatbook/announcement/controller/AmenityController.java
@@ -1,7 +1,22 @@
 package flatbook.announcement.controller;
 
+import flatbook.announcement.entity.Amenity;
+import flatbook.announcement.service.AmenityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/amenity")
 public class AmenityController {
+    @Autowired
+    private AmenityService amenityService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Amenity> getAllAnnouncement() {
+        return amenityService.getAllAmenity();
+    }
 }
--- src/main/java/flatbook/announcement/service/AmenityService.java
+++ src/main/java/flatbook/announcement/service/AmenityService.java
@@ -1,7 +1,18 @@
 package flatbook.announcement.service;
 
+import flatbook.announcement.dao.AmenityDao;
+import flatbook.announcement.entity.Amenity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AmenityService {
+    @Autowired
+    private AmenityDao amenityDao;
+
+    public List<Amenity> getAllAmenity() {
+        return amenityDao.findAll();
+    }
 }
