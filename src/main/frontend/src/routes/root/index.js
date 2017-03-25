0541c7fbecada8fc7c0fbb63dfe7b5900f419223
Vladyslav Kovtunenko <themarlboro98@gmail.com>
2017-03-25 13:26:52 +0200

    #40 update react-router

--- src/main/frontend/src/routes/root/index.js
+++ src/main/frontend/src/routes/root/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link} from 'react-router-dom';
 
 export default () =>
     <div style={{marginLeft: '50px', fontSize: '20px'}}>
