0b20805e08c0da0757ce0aae4132d654992ebfa3
juliakukuliak <juliakukuliak@gmail.com>
2017-03-28 10:39:05 -0700

    #27 styles for comment component added

--- src/main/frontend/src/components/comment/index.js
+++ src/main/frontend/src/components/comment/index.js
@@ -1,6 +1,7 @@
-import React, {PropTypes, Component} from 'react';
+import React, {Component} from 'react';
 import Button, {ButtonTypes, ButtonSizes} from '../button';
 import Textarea from '../textarea';
+import {noop} from 'lodash';
 import './comment.scss';
 
 class Comment extends Component {
@@ -8,23 +9,25 @@ class Comment extends Component {
         super(props);
         this.state = {
             value: ''
-        }
-    };
+        };
+    }
 
     onInputChange = e => this.setState({value: e.target.value});
 
     render() {
         return (
-            <div>
+            <div className="comment">
                 <Textarea
                     placeholder="Leave your comment..."
                     onChange={this.onInputChange}
                     value={this.state.value}
                 />
                 <Button
+                    className="btn-submit"
                     type={ButtonTypes.info}
                     size={ButtonSizes.medium}
                     caption="Submit"
+                    onClick={noop}
                 />
             </div>
         );
