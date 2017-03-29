b0d81225bc1eb2c228938925604840d5248b21cf
Vladyslav Kovtunenko <themarlboro98@gmail.com>
2017-03-29 19:17:08 +0300

    #16 fixed scroll bug

--- src/main/frontend/src/components/modal/index.js
+++ src/main/frontend/src/components/modal/index.js
@@ -1,27 +1,39 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import classNames from 'classnames';
 import './modal.scss';
 
-const Modal = props => {
-    const {
-        isOpen,
-        close,
-        children
-    } = props;
+class Modal extends Component {
 
-    const modalClassName = classNames('modal', {
-        ['modal-open']: isOpen
-    });
+    componentWillUpdate(nextProps) {
+        if (nextProps.isOpen) {
+            document.body.classList.add('scrollbar-disabled');
+        } else {
+            document.body.classList.remove('scrollbar-disabled');
+        }
+    };
 
-    return (
-        <div className={modalClassName}>
-            <div className="modal-content">
-                <span className="modal-close" onClick={close}><i className="fa fa-times"/></span>
-                {children}
+    render() {
+
+        const {
+            isOpen,
+            close,
+            children
+        } = this.props;
+
+        const modalClassName = classNames('modal', {
+            ['modal-open']: isOpen
+        });
+
+        return (
+            <div className={modalClassName}>
+                <div className="modal-content">
+                    <span className="modal-close" onClick={close}><i className="fa fa-times"/></span>
+                    {children}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 Modal.propTypes = {
     isOpen: PropTypes.bool.isRequired,
