9c3de960b0dd9d7b66f8d8bc303dd54adcee468d
andrey <kaonvdarley@gmail.com>
2017-03-30 16:53:49 +0300

    #35 in memory

---
+++ src/main/java/flatbook/profile/controller/SignUpController.java
@@ -0,0 +1,7 @@
+package flatbook.profile.controller;
+
+/**
+ * Created by USER on 3/30/2017.
+ */
+public class SignUpController {
+}
---
+++ src/main/java/flatbook/profile/service/TestService.java
@@ -0,0 +1,7 @@
+package flatbook.profile.service;
+
+/**
+ * Created by USER on 3/29/2017.
+ */
+public class TestService {
+}
