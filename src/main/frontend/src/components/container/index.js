f4e32d1fbf9c5c3adecf2e77aad1d21aea21ddd5
juliakukuliak <juliakukuliak@gmail.com>
2017-03-31 10:53:27 -0700

    #60 container component improved

--- src/main/frontend/src/components/container/index.js
+++ src/main/frontend/src/components/container/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './container.scss';
 
-const Container = props => {
-    return (
+const Container = ({sidebar, navbar, footer, children}) => (
         <div className="main-container">
-            {props.sidebar ? <div className="sidebar-wrapper">{props.sidebar}</div> : false}
+            {navbar && <div className="navbar-wrapper">{navbar}</div>}
+            {sidebar && <div className="sidebar-wrapper">{sidebar}</div>}
             <div className="content-wrapper">
-                {props.children}
+                {children}
             </div>
+            {footer && <div className="footer-wrapper">{footer}</div>}
         </div>
     );
-};
 
 export default Container;
