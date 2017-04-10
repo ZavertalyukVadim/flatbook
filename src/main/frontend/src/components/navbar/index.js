82418829dc3df5d2d00f9930ee69aa8e96b31435
Vladyslav Kovtunenko <themarlboro98@gmail.com>
2017-04-10 15:37:48 +0300

    added navigation for navbar

--- src/main/frontend/src/components/navbar/index.js
+++ src/main/frontend/src/components/navbar/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './navbar.scss';
 
 const Navbar = () => {
     return (
-        <div className="navbar" />
+        <div className="navbar">
+            <Link to="/" className="navbar-logo">
+                <img src="http://spd-university.com/images/logo-header.png"/>
+            </Link>
+
+            <div className="navbar-links">
+                <Link to="/showroom" className="navbar-link">Showroom</Link>
+                <Link to="/signin" className="navbar-link">Sign in</Link>
+                <Link to="/signup" className="navbar-link">Sign up</Link>
+                <Link to="/profile" className="navbar-link">
+                    <span>Profile</span>
+                    <i className="fa fa-user-circle-o navbar-link-icon"/>
+                </Link>
+                <div className="navbar-link">
+                    Sign out
+                </div>
+            </div>
+        </div>
     );
 };
 
