5c16b2c96fd1fb82621d81a2a735472ec5aa895d
Zavertalyuk_Vadim <zavertalyuk.v@gmail.com>
2017-04-12 19:47:06 +0300

    add name for db

--- src/main/java/flatbook/announcement/entity/Role.java
+++ src/main/java/flatbook/announcement/entity/Role.java
@@ -1,5 +1,6 @@
 package flatbook.announcement.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ public class Role {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
     public Role() {
