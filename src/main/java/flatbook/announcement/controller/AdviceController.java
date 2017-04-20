133aa958cd5e5d4164f171da5f7382dd815f058d
andrey <kaonvdarley@gmail.com>
2017-04-20 20:26:50 +0300

    remove advice controller

--- src/main/java/flatbook/announcement/controller/AdviceController.java
+++ src/main/java/flatbook/announcement/controller/AdviceController.java
@@ -1,82 +1,82 @@
-package flatbook.announcement.controller;
-
-import flatbook.config.ValidationErrorDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Locale;
-
-@ControllerAdvice
-public class AdviceController {
-    private static final String DEFAULT_ERROR_VIEW = "error";
-
-    private final MessageSource messageSource;
-
-    @Autowired
-    public AdviceController(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
-
-    @ExceptionHandler(value = Exception.class)
-    public ModelAndView
-    defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        if (AnnotationUtils.findAnnotation
-                (e.getClass(), ResponseStatus.class) != null)
-            throw e;
-
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", e);
-        mav.addObject("url", req.getRequestURL());
-        mav.setViewName(DEFAULT_ERROR_VIEW);
-        return mav;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ValidationErrorDTO processValidationError(MethodArgumentNotValidException ex) {
-        BindingResult result = ex.getBindingResult();
-        List<FieldError> fieldErrors = result.getFieldErrors();
-
-        return processFieldErrors(fieldErrors);
-    }
-
-    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
-        ValidationErrorDTO dto = new ValidationErrorDTO();
-
-        for (FieldError fieldError: fieldErrors) {
-            String localizedErrorMessage = resolveLocalizedErrorMessage(fieldError);
-            dto.addFieldError(fieldError.getField(), localizedErrorMessage);
-        }
-
-        return dto;
-    }
-
-    private String resolveLocalizedErrorMessage(FieldError fieldError) {
-        Locale currentLocale =  LocaleContextHolder.getLocale();
-        String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
-
-        //If the message was not found, return the most accurate field error code instead.
-        //You can remove this check if you prefer to get the default error message.
-        if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
-            String[] fieldErrorCodes = fieldError.getCodes();
-            localizedErrorMessage = fieldErrorCodes[0];
-        }
-
-        return localizedErrorMessage;
-    }
-
-}
+//package flatbook.announcement.controller;
+//
+//import flatbook.config.ValidationErrorDTO;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.MessageSource;
+//import org.springframework.context.i18n.LocaleContextHolder;
+//import org.springframework.core.annotation.AnnotationUtils;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.List;
+//import java.util.Locale;
+//
+//@ControllerAdvice
+//public class AdviceController {
+//    private static final String DEFAULT_ERROR_VIEW = "error";
+//
+//    private final MessageSource messageSource;
+//
+//    @Autowired
+//    public AdviceController(MessageSource messageSource) {
+//        this.messageSource = messageSource;
+//    }
+//
+//    @ExceptionHandler(value = Exception.class)
+//    public ModelAndView
+//    defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+//        if (AnnotationUtils.findAnnotation
+//                (e.getClass(), ResponseStatus.class) != null)
+//            throw e;
+//
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("exception", e);
+//        mav.addObject("url", req.getRequestURL());
+//        mav.setViewName(DEFAULT_ERROR_VIEW);
+//        return mav;
+//    }
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ResponseBody
+//    public ValidationErrorDTO processValidationError(MethodArgumentNotValidException ex) {
+//        BindingResult result = ex.getBindingResult();
+//        List<FieldError> fieldErrors = result.getFieldErrors();
+//
+//        return processFieldErrors(fieldErrors);
+//    }
+//
+//    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
+//        ValidationErrorDTO dto = new ValidationErrorDTO();
+//
+//        for (FieldError fieldError: fieldErrors) {
+//            String localizedErrorMessage = resolveLocalizedErrorMessage(fieldError);
+//            dto.addFieldError(fieldError.getField(), localizedErrorMessage);
+//        }
+//
+//        return dto;
+//    }
+//
+//    private String resolveLocalizedErrorMessage(FieldError fieldError) {
+//        Locale currentLocale =  LocaleContextHolder.getLocale();
+//        String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
+//
+//        //If the message was not found, return the most accurate field error code instead.
+//        //You can remove this check if you prefer to get the default error message.
+//        if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
+//            String[] fieldErrorCodes = fieldError.getCodes();
+//            localizedErrorMessage = fieldErrorCodes[0];
+//        }
+//
+//        return localizedErrorMessage;
+//    }
+//
+//}
