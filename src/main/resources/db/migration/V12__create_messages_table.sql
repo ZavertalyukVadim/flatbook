459a3b9b8351e756600109a29176c7ede6d3627f
Zavertalyuk_Vadim <zavertalyuk.v@gmail.com>
2017-04-20 21:53:01 +0300

    change migrations

--- src/main/resources/db/migration/V12__create_messages_table.sql
+++ src/main/resources/db/migration/V12__create_messages_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `team5`.`messages` (
+CREATE TABLE `messages` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `sender_id` INT NOT NULL,
   `receiver_id` INT NOT NULL,
@@ -8,16 +8,16 @@ CREATE TABLE `team5`.`messages` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_messages_user_sender`
   FOREIGN KEY (`sender_id`)
-  REFERENCES `team5`.`users` (`id`)
+  REFERENCES `users` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
   CONSTRAINT `fk_messages_user_receiver`
   FOREIGN KEY (`receiver_id`)
-  REFERENCES `team5`.`users` (`id`)
+  REFERENCES `users` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
   CONSTRAINT `fk_messages_announcement`
   FOREIGN KEY (`announcement_id`)
-  REFERENCES `team5`.`announcements` (`id`)
+  REFERENCES `announcements` (`id`)
   ON DELETE CASCADE
   ON UPDATE CASCADE);
